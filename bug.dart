8645824e38a42627655c21345ed255b29e1260b2
BugHunter-X <askzenai@gmail.com>
2024-12-22 18:50:04 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,13 @@
+```dart
+class MyClass {
+  int? _myVariable;
+
+  MyClass() {
+    _myVariable = 0; // Initialize here
+  }
+
+  void myMethod() {
+    print(_myVariable); // Accessing _myVariable here
+  }
+}
+```
